b624b35a2ac7c44935571ad155fffe17d0099a7f
Henry <pienry3g@gmail.com>
2018-11-19 04:31:47 +0100

    Update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@
 //   });
 
 // app.post('/api/users', usersController.addUser)
+// }// const usersController = require('../controllers').users
+
+// module.exports = (app) => {
+//   app.get('/api'), (req, res) => res.status(200).send({
+//     message: 'Welcome to Blog App API'
+//   });
+
+// app.post('/api/users', usersController.addUser)
 // }
