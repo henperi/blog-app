ddb84b4bca7cdc1f093c11abf073ac020a684b81
Henry <pienry3g@gmail.com>
2018-11-19 04:32:21 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,20 @@
 //   });
 
 // app.post('/api/users', usersController.addUser)
+// }// const usersController = require('../controllers').users
+
+// module.exports = (app) => {
+//   app.get('/api'), (req, res) => res.status(200).send({
+//     message: 'Welcome to Blog App API'
+//   });
+
+// app.post('/api/users', usersController.addUser)
+// }// const usersController = require('../controllers').users
+
+// module.exports = (app) => {
+//   app.get('/api'), (req, res) => res.status(200).send({
+//     message: 'Welcome to Blog App API'
+//   });
+
+// app.post('/api/users', usersController.addUser)
 // }
